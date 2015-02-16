887766e47f38d577c504c1757d24c7de501f4741
Shredderroy <amit-roy-business@outlook.com>
2015-02-16 12:26:13 -0800

    Committing the latest changes

--- DataStructures/DataStructures/Script.fsx
+++ DataStructures/DataStructures/Script.fsx
@@ -2,28 +2,28 @@
 open DataStructures
 open System.Diagnostics
 
-let d = 4
+let dim = 4
 
-let nV = 1024000 * 5
+let numOfVecs = 1024000 * 25
 
-let xV = 128
+let maxVal = 128
 
-let vA = KDTree.genRandIntVecsArr d nV xV
+let vecsArr = KDTree.genRandIntVecsArr dim numOfVecs maxVal
 
-let kDT = KDTree.buildKDTree vA
+let kDT = KDTree.buildKDTree vecsArr
 
-let tV = KDTree.genRandIntVec d xV
+let testVec = KDTree.genRandIntVec dim maxVal
 
 let sW = Stopwatch()
 sW.Start()
-let bM = KDTree.getBestMatch kDT.Value tV KDTree.distSq
+let bM = KDTree.getBestMatch kDT.Value testVec KDTree.distSq
 sW.Stop()
-printfn "bM = %A; bMD = %A" bM (KDTree.distSq tV bM)
+printfn "bM = %A; bMD = %A" bM (KDTree.distSq testVec bM)
 printfn "Time taken with a k-d tree: %A ms" sW.ElapsedMilliseconds
 
 sW.Reset()
 sW.Start()
-let bML = KDTree.getBestMatchByLinearScanning vA tV KDTree.distSq
+let bML = KDTree.getBestMatchByLinearScanning vecsArr testVec KDTree.distSq
 sW.Stop()
-printfn "bML = %A; bMLD = %A" bML (KDTree.distSq tV bML)
+printfn "bML = %A; bMLD = %A" bML (KDTree.distSq testVec bML)
 printfn "Time taken with linear scanning: %A ms" sW.ElapsedMilliseconds
