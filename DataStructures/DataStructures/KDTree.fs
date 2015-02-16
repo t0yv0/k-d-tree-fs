19289f360c6fee5320a8c40a0218a8e0132b419a
Shredderroy <amit-roy-business@outlook.com>
2015-02-16 12:22:00 -0800

    Renamed variables

--- DataStructures/DataStructures/KDTree.fs
+++ DataStructures/DataStructures/KDTree.fs
@@ -22,71 +22,78 @@ module KDTree =
     let rnd = Random()
 
 
-    let genRandIntVec (d : int) (xV : int) : IntVec =
-        Array.init d (fun s -> rnd.Next() % xV)
+    let genRandIntVec (dim : int) (maxVal : int) : IntVec =
+        Array.init dim (fun s -> rnd.Next() % maxVal)
 
 
-    let genRandIntVecsArr (d : int) (nV : int) (xV : int) : array<IntVec> =
-        Array.init nV (fun s -> genRandIntVec d xV)
+    let genRandIntVecsArr (dim : int) (numOfVecs : int) (maxVal : int) : array<IntVec> =
+        Array.init numOfVecs (fun s -> genRandIntVec dim maxVal)
 
 
-    let buildKDTree (vA : array<IntVec>) : option<KDTreeNode> =
-        let rec buildKDTreeHelper (d : int) (vA2 : array<IntVec>) : option<KDTreeNode> =
-            if vA2.Length > 1 then
-                let cSX = d % vA2.[0].Length
-                let sVA = vA2 |> Array.sortBy (fun s -> s.[cSX])
-                let mI = sVA.Length / 2
+    let buildKDTree (vecsArr : array<IntVec>) : option<KDTreeNode> =
+        let rec buildKDTreeHelper (depth : int) (vecsArr2 : array<IntVec>) : option<KDTreeNode> =
+            if vecsArr2.Length > 1 then
+                let currSplittingAxis = depth % vecsArr2.[0].Length
+                let sVA = vecsArr2 |> Array.sortBy (fun s -> s.[currSplittingAxis])
+                let medIndex = sVA.Length / 2
                 {
-                KDTreeNode.LeftChild = buildKDTreeHelper (d + 1) (sVA.[..(mI - 1)]);
-                KDTreeNode.RightChild = buildKDTreeHelper (d + 1) (sVA.[(mI + 1)..]);
-                KDTreeNode.SplittingAxis = cSX;
-                KDTreeNode.NodeVal = sVA.[mI]} |> Some
-            elif vA2.Length = 1 then
-                let cSX = d % vA2.[0].Length
+                KDTreeNode.LeftChild = buildKDTreeHelper (depth + 1) (sVA.[..(medIndex - 1)]);
+                KDTreeNode.RightChild = buildKDTreeHelper (depth + 1) (sVA.[(medIndex + 1)..]);
+                KDTreeNode.SplittingAxis = currSplittingAxis;
+                KDTreeNode.NodeVal = sVA.[medIndex]} |> Some
+            elif vecsArr2.Length = 1 then
+                let cSX = depth % vecsArr2.[0].Length
                 {
                 KDTreeNode.LeftChild = None;
                 KDTreeNode.RightChild = None;
                 KDTreeNode.SplittingAxis = cSX;
-                KDTreeNode.NodeVal = vA2.[0]} |> Some
+                KDTreeNode.NodeVal = vecsArr2.[0]} |> Some
             else
                 None
-        buildKDTreeHelper 0 vA
+        buildKDTreeHelper 0 vecsArr
 
 
-    let distSq (v1 : IntVec) (v2 : IntVec) : int =
-        (v1, v2)
+    let distSq (vec1 : IntVec) (vec2 : IntVec) : int =
+        (vec1, vec2)
         ||> Array.map2 (fun s t -> let u = s - t in u * u)
         |> Array.sum
 
 
-    let getBestMatch (kDT : KDTreeNode) (tV : IntVec) (dF : IntVec -> IntVec -> int) : IntVec =
-        let rec getCandBestMatch (cKDT : KDTreeNode) (tV2 : IntVec) (pA : list<KDTreeNode>) : KDTreeNode =
-            match tV2.[cKDT.SplittingAxis] < cKDT.NodeVal.[cKDT.SplittingAxis], cKDT.LeftChild, cKDT.RightChild with
-            | true, Some lC, _ | false, Some lC, None -> getCandBestMatch lC tV2 (cKDT :: pA)
-            | true, None, Some rC | false, _, Some rC -> getCandBestMatch rC tV2 (cKDT :: pA)
-            | _ -> checkCandBestMatch cKDT tV2 pA
-        and checkCandBestMatch (cBMN : KDTreeNode) (tV2 : IntVec) (pA : list<KDTreeNode>) : KDTreeNode =
-            match pA with
-            | [] -> cBMN
-            | pN :: t ->
-                let cBMD = dF tV2 cBMN.NodeVal
-                let pD = dF tV2 pN.NodeVal
-                let pSD = tV2.[pN.SplittingAxis] - pN.NodeVal.[pN.SplittingAxis]
-                let pSDSq = pSD * pSD
-                match cBMD <= pD, cBMD < pSDSq, pD < pSDSq with
+    let getBestMatch (kDT : KDTreeNode) (testVec : IntVec) (normFunc : IntVec -> IntVec -> int) : IntVec =
+        let rec getCandBestMatch (currKDT : KDTreeNode) (testVec2 : IntVec) (parAcc : list<KDTreeNode>) : KDTreeNode =
+            match (
+                    testVec2.[currKDT.SplittingAxis] < currKDT.NodeVal.[currKDT.SplittingAxis],
+                    currKDT.LeftChild, currKDT.RightChild) with
+            | true, Some leftChild, _ | false, Some leftChild, None -> getCandBestMatch leftChild testVec2 (currKDT :: parAcc)
+            | true, None, Some rightChild | false, _, Some rightChild -> getCandBestMatch rightChild testVec2 (currKDT :: parAcc)
+            | _ -> checkCandBestMatch currKDT testVec2 parAcc
+        and checkCandBestMatch (currBestMatchNode : KDTreeNode) (testVec2 : IntVec) (parAcc : list<KDTreeNode>) : KDTreeNode =
+            match parAcc with
+            | [] -> currBestMatchNode
+            | parNode :: t ->
+                let currBestMatchDist = normFunc testVec2 currBestMatchNode.NodeVal
+                let parDist = normFunc testVec2 parNode.NodeVal
+                let planeSplittingDist = testVec2.[parNode.SplittingAxis] - parNode.NodeVal.[parNode.SplittingAxis]
+                let planeSplittingDistSq = planeSplittingDist * planeSplittingDist
+                match (
+                        currBestMatchDist <= parDist,
+                        currBestMatchDist < planeSplittingDistSq,
+                        parDist < planeSplittingDistSq) with
                 | (a, b, c) when (a = true && b = false) || (a = false && c = false) ->
-                    let (tN, tD) = if a then (cBMN, cBMD) else (pN, pD)
-                    let nBMN =
-                        if pN.RightChild.IsSome && pSD < 0 then getCandBestMatch pN.RightChild.Value tV2 []
-                        elif pN.LeftChild.IsSome && pSD >= 0 then getCandBestMatch pN.LeftChild.Value tV2 []
+                    let (tN, tD) = if a then (currBestMatchNode, currBestMatchDist) else (parNode, parDist)
+                    let testBestMatchNode =
+                        if parNode.RightChild.IsSome && planeSplittingDist < 0 then getCandBestMatch parNode.RightChild.Value testVec2 []
+                        elif parNode.LeftChild.IsSome && planeSplittingDist >= 0 then getCandBestMatch parNode.LeftChild.Value testVec2 []
                         else tN
-                    if (dF tV2 nBMN.NodeVal) < tD then checkCandBestMatch nBMN tV2 t else checkCandBestMatch tN tV2 t
-                | true, true, _ -> checkCandBestMatch cBMN tV2 t
-                | _ -> checkCandBestMatch pN tV2 t
-        (getCandBestMatch kDT tV []).NodeVal
+                    if (normFunc testVec2 testBestMatchNode.NodeVal) < tD then
+                        checkCandBestMatch testBestMatchNode testVec2 t
+                    else checkCandBestMatch tN testVec2 t
+                | true, true, _ -> checkCandBestMatch currBestMatchNode testVec2 t
+                | _ -> checkCandBestMatch parNode testVec2 t
+        (getCandBestMatch kDT testVec []).NodeVal
 
 
-    let getBestMatchByLinearScanning (vA : array<IntVec>) (tV : IntVec) (dF : IntVec -> IntVec -> int) : IntVec =
-        (vA |> Array.sortBy (fun s -> dF s tV)).[0]
+    let getBestMatchByLinearScanning (vecsArr : array<IntVec>) (testVec : IntVec) (normFunc : IntVec -> IntVec -> int) : IntVec =
+        (vecsArr |> Array.sortBy (fun s -> normFunc s testVec)).[0]
 
         
